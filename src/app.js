e916c363ea0a8eb046e1fc8e7b3081d72151b901
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-02-14 12:59:21 +0330

    create a Cors Policy middelware

--- src/app.js
+++ src/app.js
@@ -1,25 +1,37 @@
-const express = require ("express")
-const path = require("path")
+const { log } = require("console");
+const express = require("express");
+const path = require("path");
+const { setHeaders } = require("./middlewares/headers");
+const app = express();
 
-const app = express()
+//* BodyParser
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+//* Cors Policy
+app.use(setHeaders);
 
-//? Static Folders
-app.use(express.static(path.join(__dirname,"..","public")))
-app.use("/css",express.static(path.join(__dirname,"public/css")))
-app.use("/js",express.static(path.join(__dirname,"public/js")))
-app.use("/fonts",express.static(path.join(__dirname,"public/fonts")))
-app.use("/images",express.static(path.join(__dirname,"public/images")))
+//* Static Folders
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use("/css", express.static(path.join(__dirname, "public/css")));
+app.use("/js", express.static(path.join(__dirname, "public/js")));
+app.use("/fonts", express.static(path.join(__dirname, "public/fonts")));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 //* Template Engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.set("view engine" , "ejs")
-app.set("views",path.join(__dirname,"views"))
+//* Routes
+app.get("/", (req, res) => {
+  return res.render("index");
+});
 
+//* 404 Error Handler
+app.use((req, res) => {
+  console.log("This Path Is Not Found", req.path);
+  return res
+    .status(401)
+    .json({ message: "404! Path Not Found . Please check The Path/Method" });
+});
 
-
-//! Routes
-app.get("/", (req , res)=>{
-    return res.render("index")
-})
-
-
-module.exports =  app
+module.exports = app;
