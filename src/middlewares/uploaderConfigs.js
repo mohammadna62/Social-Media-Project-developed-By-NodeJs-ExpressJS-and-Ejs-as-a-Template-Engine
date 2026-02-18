4cbe62daeae0cad460f2ec33e8de10de67bddab2
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-02-18 14:24:07 +0330

    create multer Feature

---
+++ src/middlewares/uploaderConfigs.js
@@ -0,0 +1,37 @@
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+exports.multerStorage = (destination, allowedTypes = /jpeg|jpg|png|webp/) => {
+  //* Create the Destination directory if it does not exist
+  if (!fs.existsSync(destination)) {
+    fs.mkdirSync(destination);
+  }
+  //* Multer Configs
+  const storge = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, destination);
+    },
+    filename: function (req, file, cb) {
+      const uniqe = Date.now() * Math.floor(Math.random() * 1e9);
+      const ext = path.extname(file.originalname);
+      cb(null, `${uniqe}${ext}`);
+    },
+  });
+  const fileFilter = function (req, file, cb) {
+    //* Allow Extension
+    if(allowedTypes.test(file.mimeTypes)){
+       cb(null, true)
+    }else{
+        cb(new Error("File Type Not Allow"))
+    }
+  };
+  const uploader = multer({
+    storage,
+    limits: {
+      fileSize: 512_000_000, //5 mB limit
+    },
+    fileFilter,
+  });
+  return upload
+};
