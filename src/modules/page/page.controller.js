f25c7af53bbaedf3c9489582e53f410a9f774e12
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-02-22 13:51:48 +0330

    coding followed feater

--- src/modules/page/page.controller.js
+++ src/modules/page/page.controller.js
@@ -1,5 +1,6 @@
 const hasAccessToPage = require("./../../utils/hasAccessToPage");
 const FollowModel = require("./../../models/Follow");
+const UserModel = require("./../../models/User");
 exports.getGage = async (req, res, next) => {
   try {
     const user = req.user;
@@ -25,18 +26,47 @@ exports.getGage = async (req, res, next) => {
     next(err);
   }
 };
-exports.follow = async (req, res,next) => {
-  try{
-    return res.json({message : " User Follwed Successfully"})
-  }catch(err){
-     next(err)
+exports.follow = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const { pageID } = req.params;
+    const targetOwnPage = await UserModel.findOne({ _id: pageID });
+    if (!targetOwnPage) {
+      req.flash("error", "Page NOt Found To Follow");
+      return res.redirect(`/pages/${pageID}`);
+    }
+     if(user._id.toString()=== pageID){
+      req.flash("error", "You Can Not Follow Yourself");
+      return res.redirect(`/pages/${pageID}`);
+     }
+    const existingFollow = await FollowModel.findOne({
+      follower: user._id,
+      following: pageID,
+    });
+    if (existingFollow) {
+      req.flash("error", "Page Already Followed");
+      return res.redirect(`/pages/${pageID}`);
+    }
+    await FollowModel.create({
+      follower :user._id ,
+      following:pageID,
+    })
+
+    
+    req.flash("success", "Page Followed Successfully");
+      return res.redirect(`/pages/${pageID}`);
+
+
+
+  } catch (err) {
+    next(err);
   }
 };
 
-exports.unfollow = async (req, res,next) => {
-  try{
-    return res.json({message : " User Unfollwed Successfully"})
-  }catch(err){
-     next(err)
+exports.unfollow = async (req, res, next) => {
+  try {
+    return res.json({ message: " User Unfollwed Successfully" });
+  } catch (err) {
+    next(err);
   }
 };
