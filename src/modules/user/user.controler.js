cfc553dfab163c34cfd0b5f2c6b8697e445ff92b
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-02-24 14:01:47 +0330

    connect manage.ejs to db

--- src/modules/user/user.controler.js
+++ src/modules/user/user.controler.js
@@ -1,13 +1,35 @@
-const UserModel = require("./../../models/User")
+const UserModel = require("./../../models/User");
 
-exports.showPageEditView = async (req , res, next)=>{
-    try{
-        const user = await UserModel.findOne({_id:req.user._id})
-        
-       return res.render('user/edit.ejs',{
-        user,
-       })
-    }catch(err){
-      next(err)
+exports.showPageEditView = async (req, res) => {
+  const user = await UserModel.findOne({ _id: req.user._id });
+
+  return res.render("user/edit.ejs", {
+    user,
+  });
+};
+exports.updateProfile = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+    //Handler File Upload
+    if (!req.file) {
+      req.flash("error", "please Upload A profile Picture");
+      return res.redirect("/users/edit-profile");
     }
-}
+    const { filename } = req.file;
+    const profilePath = `image/profiles/${filename}`;
+    const user = await UserModel.findOneAndUpdate(
+      { _id: userId },
+      { profilePicture: profilePath },
+      { new: true }// return Updated user document
+    );
+    if(!user){
+      req.flash("error", "User Not Found");
+    return res.redirect("/users/edit-profile");
+    }
+    req.flash("success", "Profile Picture Updated Successfully");
+    return res.redirect("/users/edit-profile");
+    
+  } catch (err) {
+    next(err);
+  }
+};
--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const controller = require("./user.controler")
 const auth = require("./../../middlewares/auth")
+const {multerStorage} = require("./../../middlewares/uploaderConfigs")
  router = new  express.Router()
-
+const upload =multerStorage("public/images/profiles")
 
 
 router.route("/edit-profile").get(auth,controller.showPageEditView)
+router.route("/profile-picture").post(auth,upload.single("profile"),controller.updateProfile)
 
  module.exports = router
