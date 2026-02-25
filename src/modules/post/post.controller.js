6e935d9a61675778c08128051dc90861af0c53bd
Mohammad Naghavi Olyaei <mohammad.naghavi@gmail.com>
2026-02-25 13:53:58 +0330

    develope like feature lagic and connect to frontend

--- src/modules/post/post.controller.js
+++ src/modules/post/post.controller.js
@@ -1,5 +1,7 @@
 const PostModel = require("./../../models/Post");
-const {createPostValidator} = require("./post.validators");
+const LikeModel = require("./../../models/Like");
+const { createPostValidator } = require("./post.validators");
+const hasAccessToPage = require("./../../utils/hasAccessToPage");
 
 exports.showPostUploadView = async (req, res) => {
   return res.render("post/upload");
@@ -14,10 +16,7 @@ exports.createPost = async (req, res, next) => {
       req.flash("error", "Media is required");
       return res.render("post/upload");
     }
-    await createPostValidator.validate(
-      { description },
-      { abortEarly: false },
-    );
+    await createPostValidator.validate({ description }, { abortEarly: false });
     const mediaUrlPath = `images/posts/${req.file.filename}`;
 
     //* Create New Post
@@ -27,24 +26,56 @@ exports.createPost = async (req, res, next) => {
         path: mediaUrlPath,
       },
       description,
-      hashtags:tags,
-      user : user._id
+      hashtags: tags,
+      user: user._id,
     });
-    await post.save()
-    req.flash("success","Post Created successfully")
-    return res.render("post/upload")
+    await post.save();
+    req.flash("success", "Post Created successfully");
+    return res.render("post/upload");
   } catch (err) {
     next(err);
   }
 };
 
-exports.like = async (req, res) => {
-  
-};
+exports.like = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const { postID } = req.body;
 
+    const post = await PostModel.findOne({ _id: postID });
+    if (!post) {
+      //! Error Message
+    }
+
+    const hasAccess = await hasAccessToPage(user._id, post.user.toString());
+    if (!hasAccess) {
+      //! Error Message
+    }
+
+    const existingLike = await LikeModel.findOne({
+      user: user._id,
+      post: postID,
+    });
+
+    if (existingLike) {
+      return res.redirect(`/pages/${post.user}`); // /page/:pageID ...
+    }
+
+    const like = new LikeModel({
+      post: postID,
+      user: user._id,
+    });
+
+    like.save();
+
+    return res.redirect(`/pages/${post.user}`);
+  } catch (err) {
+    next(err);
+  }
+};
 exports.dislike = async (req, res, next) => {
   try {
-    return res.render("post/upload")
+    return res.render("post/upload");
   } catch (err) {
     next(err);
   }
